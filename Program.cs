c1de8947857bdd7ebfbfcbb6418a064d699a7bff
madmuddy <mmamma496@gmail.com>
2021-10-01 04:26:21 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+using System.Threading;
+
+namespace FactorioSplitScreen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process.Start("UniversalSplitScreen.exe");
+            Thread.Sleep(5000);
+            Process.Start("SplitScreen1.bat");
+            Thread.Sleep(10000);
+            Process.Start("SplitScreen2.bat");
+        }
+    }
+}
